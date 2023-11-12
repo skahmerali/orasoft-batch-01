0b2a469644462831ff65c0123ab17c6d175a223e
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-11-12 17:28:56 +0500

    javascript class 2 and 3

---
+++ javaScript/2. class/index.js
@@ -0,0 +1,71 @@
+// var  num = 12 
+// var result = num % 5
+
+// console.log(result)
+
+
+// var num = 2;
+//  num = num + 1;
+// console.log(num);
+
+// var firstname = 'zubair ';
+// firstname = 'ahmer'
+
+// var num = 5
+// console.log("num1 " + num);
+
+// var newnum = ++num
+// console.log("newnum1 " + newnum)
+// console.log("num2 " + num)
+
+// num = ++num
+// console.log(num)
+
+
+// var num = 3
+// console.log("num1 ", num) // 3
+
+// var num1 = --num
+// console.log("num2 ", num1) // 2 , 2
+
+// var num2 = num++
+// console.log("num3 ", num2) // 3 , 2 
+
+// var num3 = ++num
+// console.log("num4 ", num3) // 4 , 5 , 3,  
+
+// var num4 = --num
+// console.log("num5 ", num4) // 3, 4, 2 , 2
+
+
+// var  num  = 3 
+
+// var result = num++ + --num - ++num + ++num  - ++num
+//         //    3    +   3   - 4     +    5   -    6
+//         //     6 + 1 - 6
+// // 1, -1,5
+// console.log(result)
+
+
+// var num = 4 + (1 + 4 ) * 4 / 5 ; // 7.2, 8, 8,
+//           4   +     5  *  0.8
+//           4 + 4
+//           8
+// console.log(num)
+
+// var firstname = prompt(`<h1> Your f'Name </h1>`);
+// var lastNamae = prompt("Your l'Name");
+// console.log(firstname + lastNamae);
+
+
+var age = prompt('Whats your age');
+
+// if (age == 10) {
+//     alert("your age is " + age)
+// }
+
+if(age == 100){
+    alert("your age is " + age)
+}
+
+
