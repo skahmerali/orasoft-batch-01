9fd431328250bed7b1babe959a74ef752b544643
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-11-18 00:43:04 +0500

    javscript class 04

---
+++ javaScript/4. class/index.js
@@ -0,0 +1,43 @@
+// var std = "xuz";
+// var std1 = 'jkfsjlf'
+// var std2 = 'jkfsjlf'
+// var std3 = 'jkfsjlf'
+
+// console.log(std)
+
+var stdname;
+var students = ["name1", "name2", "name3", "name4 ", "name5"];
+
+// console.log("fjsl", students[0])
+// console.log(students.length)
+// console.log(students[3])
+
+// students[6] = "name33"
+// console.log(students)
+// console.log(students[5])
+
+// students.pop();
+// console.log(students)
+// students.pop();
+// console.log(students)
+
+// students.push('name3', "name4", "name5")
+// console.log(students)
+
+// console.log(students)
+// students.shift()
+// console.log(students)
+
+// students.unshift('name0', "name2")
+// console.log(students)
+
+// console.log(students)
+// students.splice(2, 0, "pig", "duck", "emu")
+// console.log(students)
+// students.splice(2, 1)
+// console.log(students)
+
+// var newStudents = students.slice(2, 4);
+// console.log(newStudents)
+
+let a  = ["ahmer1", "ahmer1",]
