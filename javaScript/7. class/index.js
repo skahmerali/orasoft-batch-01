04bc0c45601fd6908ca17c9397c8c09f2d6ac0a3
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-11-29 20:54:47 +0500

    js class 07

---
+++ javaScript/7. class/index.js
@@ -0,0 +1,67 @@
+// var name = 'zubair';
+// var name = 'ahmer';
+
+// var username;
+// username = 'zuvair ';
+
+// var username = "ahmer";
+// console.log(username);
+
+
+// let username = "zubair";
+// username = "ahmer "
+// console.log(username)
+
+
+// const username = 'zubair';
+// const username = "ahmer";
+// username = 'zubair'
+// var fruit = 'NO Fruid'
+
+// let randomnumber = 4
+// if (randomnumber === 3) {
+//     var fruit = 'Apple'
+
+// } else if (randomnumber === 2) {
+
+//     var fruit = 'Orange'
+// } else {
+
+//     console.log(fruit)
+// }
+
+// let username = 'zubair '
+
+// if (username === 'zubair') {
+//     var abc = "kfjdalskf"
+//     if (nationality === "pakistan ") {
+//         var abc = "kfjdalskf"
+//     }
+// }
+
+
+// var cityname = prompt('enter your city name ');
+// var cityArr = ['karachi', 'lahore', 'punjab']
+// for (let i = 0; i < cityArr.length; i++) {
+//     const element = cityArr[i];
+//     console.log("element", element, cityname.toLocaleLowerCase())
+
+
+//     if (cityname.toLocaleLowerCase() === element) {
+//         console.log('city is exist');
+//         break
+//     } else {
+//         console.log('city not  is exist');
+
+//     }
+
+
+// }
+
+
+// var fruit = "this is fruit"
+// // console.log(fruit.length)
+
+// console.log(fruit.charAt(1))
+
+// console.log(fruit.replaceAll("is", 'on'))
