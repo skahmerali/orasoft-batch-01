2fd19723a6d5ecdfcebb00ecfe5fdb9446ed099a
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-12-17 21:08:52 +0500

    all classes

---
+++ javaScript/class 11/Dom/app.js
@@ -0,0 +1,4 @@
+// console.log(document);
+
+console.log(document.childNodes[1].childNodes[0].childNodes[5].childNodes[0].parentNode.parentNode.parentNode.childNodes[2].lastChild.previousSibling.previousSibling.previousSibling.previousSibling.nextSibling.nextSibling)
+// console.log(document.childNodes[1].childNodes[2].childNodes[1].childNodes[0])
