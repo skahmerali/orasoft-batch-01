f8173aa23ba706a6a09a3ec82f68c7e725976bf1
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-12-25 11:46:49 +0500

    class 13

---
+++ javaScript/13. class/index.js
@@ -0,0 +1,78 @@
+// let classes = ["gfdgdf", 'jfdsklafjsdf']
+// let student = {
+//     name : 'fjlasd'
+// }
+
+// let student = {
+//     firstName: 'Muhammad',
+//     lastName: 'Zubair',
+//     class: 10,
+//     roll: 12121,
+//     fullName: function () {
+//         console.log(this.firstName + " " + this.lastName)
+//     }
+// };
+// let student1 = {
+//     firstName: 'Bilal',
+//     lastName: 'Ahmed',
+//     class: 10,
+//     roll: 12121
+// };
+
+// const stutedfistNme = student.firstName
+// student.age = 22
+// console.log(student.fullName())
+
+
+// let allstudend = [
+//     {student1name : 'nJSDKA'},
+//     {student1name : 'nJSDKA'},
+//     {student1name : 'nJSDKA'},
+//     {student1name : 'nJSDKA'},
+//     {student1name : 'nJSDKA'},
+//     {student1name : 'nJSDKA'},
+// ]
+
+// let allstudents = []
+
+// function signUp() {
+//     let data = {
+//         firsName: document.getElementById('fname').value,
+//         firsName: document.getElementById('fname').value,
+//         firsName: document.getElementById('fname').value,
+//         firsName: document.getElementById('fname').value,
+//         firsName: document.getElementById('fname').value,
+//     }
+
+//     allstudents.push(data)
+// }
+
+
+// function Login() {
+
+// }
+
+let student1 = {
+    firstName: 'Bilal',
+    lastName: 'Ahmed',
+    class: 10,
+    roll: 12121
+};
+
+// student1.age = 22;
+console.log(student1)
+
+// let Student = function (firsName, lastName) {
+
+//     this.firsName = firsName;
+//     this.lastName = lastName;
+// }
+// let student = new Student("firsName", "lastName")
+Student.prototype.flullname = function () {
+
+}
+// // student.age = 23
+// console.log(student.age)
+
+// let student2 = new Student("firsName", "lastName")
+// console.log(student2)
