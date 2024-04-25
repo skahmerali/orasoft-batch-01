4d4ff075fa5d698ebaf1b901d07edb33671a7147
zubairkhan1234 <zubairsaleem1210@gmail.com>
2024-04-25 21:17:31 +0500

    class 5

--- server/class 4/server.js
+++ server/class 4/server.js
@@ -1,21 +1,58 @@
 const express = require("express");
+var bodyParser = require("body-parser");
 const cors = require("cors");
 const fs = require("fs");
-const app = express();
-const db = "mongodb+srv://orasoft:orasoft12345@cluster0.mxddhps.mongodb.net/";
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+const db = process.env.MONGO_DB;
+let PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(bodyParser.json());
+
 const Schema = mongoose.Schema;
+
+mongoose.connect(process.env.MONGO_DB).then(() => console.log("Connected!"));
+
 const SignupModel = new Schema({
   email: String,
-  DOB: Date,
+  DOB: String,
   name: String,
   fatherName: String,
   gender: String,
   password: String,
 });
 const SomeModel = mongoose.model("singup", SignupModel);
-app.post("/signup", (req, res) => {
-  const { name, fatherName, DOB, gender, email, password } = req.body;
 
+app.post("/signup", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { email, DOB, name, fatherName, gender, password } = req.body;
+    console.log(email, 'email');
+    const result = new SomeModel({
+      email: email,
+      DOB: DOB,
+      name: name,
+      fatherName: fatherName,
+      gender: gender,
+      password: password,
+    });
+    result.save();
+    res.send({
+      status: 200,
+      message :'Successfully save the data'
+    })
+
+  } catch {
+    res.send({
+      status: 404,
+      message :'Fail to save the data'
+    })
+  }
   // const email = req.body;
+});
+
+app.listen(PORT, function () {
+  console.log("server is lisning on the post of  " + PORT);
 });
