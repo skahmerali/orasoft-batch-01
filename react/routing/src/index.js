23ea1f9e40e301050ea66508a70146adb3e86bd6
zubairkhan1234 <zubairsaleem1210@gmail.com>
2024-04-28 22:03:06 +0500

    revert routing project code

--- react/routing/src/index.js
+++ react/routing/src/index.js
@@ -1,70 +1,61 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// // import App from "./App";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Home from "./components/home/home";
-// import About from "./components/about";
-// import Contact from "./components/contact/contact";
-// import NotFound from "./components/notFound/NotFound";
-// import Products from "./components/products/products";
-// import ProductDetail from "./components/productDetail/ProductDetail";
-// import Memo from "./components/memo/Memo";
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/home/home";
+import About from "./components/about";
+import Contact from "./components/contact/contact";
+import NotFound from "./components/notFound/NotFound";
+import Products from "./components/products/products";
+import ProductDetail from "./components/productDetail/ProductDetail";
+import Memo from "./components/memo/Memo";
 
 import SignUp from "./connectserver/signup";
 
-// let router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Home />,
-//   },
-//   {
-//     path: "/about",
-//     element: <About />,
-//   },
-//   {
-//     path: "/contact",
-//     element: <Contact />,
-//   },
-//   {
-//     path: "/products",
-//     element: <Products />,
-//     loader: () => {
-//       let allposts = fetch("https://jsonplaceholder.typicode.com/photos")
-//         .then((response) => response.json())
-//         .then((json) => json);
-//       return allposts;
-//     },
-//   },
-//   {
-//     path: "/product-detail/:productid",
-//     element: <ProductDetail />,
-//   },
-//   {
-//     path: "/memo",
-//     element: <Memo />,
-//   },
-//   {
-//     path: "*",
-//     element: <NotFound />,
-//   },
-// ]);
+let router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+    loader: () => {
+      let allposts = fetch("https://jsonplaceholder.typicode.com/photos")
+        .then((response) => response.json())
+        .then((json) => json);
+      return allposts;
+    },
+  },
+  {
+    path: "/product-detail/:productid",
+    element: <ProductDetail />,
+  },
+  {
+    path: "/memo",
+    element: <Memo />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
-// // {
-// //   /* <App /> */
-// // }
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
-
-import React from "react";
-import ReactDOM from "react-dom/client";
+// {
+//   /* <App /> */
+// }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <SignUp />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
